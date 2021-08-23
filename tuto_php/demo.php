4e4993decfd246029dddfa971fbfb7ad557aed7a
ZafAkpla <zafakpla@gmail.com>
2021-08-23 18:10:59 +0100

    apprentissage des tableaux en php

--- tuto_php/demo.php
+++ tuto_php/demo.php
@@ -1,8 +1,2 @@
 <?php
-
-$prenom = 'Marc';
-$nom = 'DOS';
-$note1 = 20;
-$note2 = 10;
-
-echo 'Bonjour ' . "$nom $prenom " . 'vous avez eu ' . ($note1 + $note2)/2 . ' de moyenne.';
+// debut d'aprentissage des conceptes de tableau en php
