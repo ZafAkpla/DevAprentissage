cf16c9c729dfaec3e8208d993991c8a816d13acd
ZafAkpla <zafakpla@gmail.com>
2021-08-23 18:52:09 +0100

    affichage des elements d'un tableau

--- tuto_php/demo.php
+++ tuto_php/demo.php
@@ -1,2 +1,11 @@
 <?php
 // debut d'aprentissage des conceptes de tableau en php
+
+$eleve = [
+    'nom' => 'Marc',
+    'prenom' => 'Dos',
+    'notes' => [10,15,20,9,7]
+];
+$moyenne = ($eleve['notes'][0]+$eleve['notes'][1]+$eleve['notes'][2]+$eleve['notes'][3]+$eleve['notes'][4])/5;
+
+echo 'Bonjour ' .$eleve['prenom'] . ' ' .$eleve['nom'] .' votre moyenne est $moyenne';
