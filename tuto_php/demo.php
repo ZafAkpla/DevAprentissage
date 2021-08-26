536509c066d3f31b253226abc139c39462ad2699
ZafAkpla <zafakpla@gmail.com>
2021-08-26 12:13:14 +0100

    fonction utilisateur en php

--- tuto_php/demo.php
+++ tuto_php/demo.php
@@ -19,5 +19,51 @@ switch ($actions) {
 }  */
 
 
-// boucle
+// boucle forEach
+
+/* $eleves = [
+    'CM1' => ['Jean','Marc','Julien','Azaf'],
+    'CM2' => ['Quet','Ivan','Lola']
+];
+
+foreach($eleves as $classe => $ListEleves){
+    echo "La classe $classe : \n";
+    foreach ($ListEleves as $eleve) {
+        echo " - $eleve \n";
+    }
+} */
+
+// remplissage et affichage d'un tableau par les saisi de l'utilisateur
+
+/* $notes = [];
+$note = null;
+while ($note !== 'fin') {
+    $note=readline('Rentrer une note ou taper fin : ');
+    $notes[]=$note;
+}
+
+echo "\nListe des notes saisi au clavier\n\n";
+foreach ($notes as $ListNote) {
+    echo "- $ListNote\n";
+} */
+
+// function utilisateur
+
+function repondre_oui_non($messag){
+    echo "$messag \n";
+    $message = "votre reponse doit est oui ou non ";
+
+    $saisi=readline('o/n');
+    if ($saisi == 'n') {
+        return false;
+    }elseif ($saisi == 'o') {
+        return true;
+    }else{
+        return $message;
+    }
+}
+
+$resultat = repondre_oui_non('voulez vous continuer ?');
+
+var_dump($resultat);
 
