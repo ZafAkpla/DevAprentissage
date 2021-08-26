919188044923d0c00de3b8c4611154ca60f1bf4f
ZafAkpla <zafakpla@gmail.com>
2021-08-26 19:35:11 +0100

    introduction au dev php avec html : reutilistion des bout de code avec include et require

---
+++ tuto_php/footer.php
@@ -0,0 +1,5 @@
+<script src="/docs/5.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+<script src="offcanvas.js"></script>
+</body>
+</html>
